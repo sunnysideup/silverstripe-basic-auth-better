61617235bae1c9f5d2ce182cb131884461b86cc9
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-01-08 11:56:37 +1300

    MNT: first commit

--- src/AddOrRemveBasicAuth.php
+++ src/AddOrRemveBasicAuth.php
@@ -6,39 +6,43 @@ class AddOrRemveBasicAuth implements Flushable
 {
     public static function flush()
     {
-        $htpasswdPath = '/container/application/.htpasswd';
-        $htaccessPaths = [
-            'public/.htaccess',
-            'public/assets.htaccess',
-        ];
+        if (Director::isTest()) {
+            $htpasswdPath = '/container/application/.htpasswd';
+            $htaccessPaths = [
+                'public/.htaccess',
+                'public/assets.htaccess',
+            ];
+            $userName = 'user';
+            $password = 'password123';
 
+            // Check if .htpasswd file exists, create it if not
+            $defaultPassword = 'user:' . password_hash('password123', PASSWORD_BCRYPT);
+            file_put_contents($htpasswdPath, $defaultPassword . PHP_EOL);
+            echo '.htpasswd file created.' . PHP_EOL;
 
-        // Check if .htpasswd file exists, create it if not
-        $defaultPassword = 'user:' . password_hash('password123', PASSWORD_BCRYPT);
-        file_put_contents($htpasswdPath, $defaultPassword . PHP_EOL);
-        echo '.htpasswd file created.' . PHP_EOL;
-
-        // Define the lines to be added
-        $htaccessLines = [
-            'AuthUserFile ' . $htpasswdPath . '.htpasswd',
-            'AuthType Basic',
-            'AuthName "My restricted Area"',
-            'Require valid-user'
-        ];
-        foreach ($htaccessPaths as $htaccessPath) {
-            // Check if .htaccess file exists, create or append to it
-            if (!file_exists($htaccessPath)) {
-                file_put_contents($htaccessPath, implode(PHP_EOL, $htaccessLines) . PHP_EOL);
-                echo '.htaccess file created and updated.' . PHP_EOL;
-            } else {
-                $existingContent = file_get_contents($htaccessPath);
-                foreach ($htaccessLines as $line) {
-                    if (!str_contains($existingContent, $line)) {
-                        file_put_contents($htaccessPath, $line . PHP_EOL, FILE_APPEND);
+            // Define the lines to be added
+            $htaccessLines = [
+                'AuthUserFile ' . $htpasswdPath . '.htpasswd',
+                'AuthType Basic',
+                'AuthName "My restricted Area"',
+                'Require valid-user'
+            ];
+            foreach ($htaccessPaths as $htaccessPath) {
+                // Check if .htaccess file exists, create or append to it
+                if (!file_exists($htaccessPath)) {
+                    file_put_contents($htaccessPath, implode(PHP_EOL, $htaccessLines) . PHP_EOL);
+                    echo '.htaccess file created and updated.' . PHP_EOL;
+                } else {
+                    $existingContent = file_get_contents($htaccessPath);
+                    foreach ($htaccessLines as $line) {
+                        if (!str_contains($existingContent, $line)) {
+                            file_put_contents($htaccessPath, $line . PHP_EOL, FILE_APPEND);
+                        }
                     }
+                    echo '.htaccess file updated.' . PHP_EOL;
                 }
-                echo '.htaccess file updated.' . PHP_EOL;
             }
+        } else {
         }
     }
 }
